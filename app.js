b3ea0714aa5056a6b0c5e922291dbb344fdb420d
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2023-09-03 19:52:41 -0400

    terminando list mercado

--- app.js
+++ app.js
@@ -7,15 +7,52 @@ document.querySelector('input[type=submit]')
     //aqui adicionamos uma ação para o botão
 
     // as variaveis para pegarmos os elementos dentro dos inputs
-    var nomeProduto = document.querySelector('input[name=nome_produto]').value;
-    var valorProduto = document.querySelector('input[name=valor_produto]').value;
+    var nomeProduto = document.querySelector('input[name=nome_produto]');
+    var valorProduto = document.querySelector('input[name=price_produto]');
 
     // aqui adicionamos os elementos pegos dentro da lista
     items.push({
-        nome:nomeProduto,
-        valor:valorProduto
+        nome:nomeProduto.value,
+        valor:valorProduto.value
     });
+    // nesse momento criamos uma variavel para pegar o elementos de lista de produtos do html 
+    let listaProdutos = document.querySelector('.lista_de_produtos')
     
-    // aqui estamos mostrando os itens da lista, em um alerta da paagina.
-    alert(items[0]);
+    // adicionanmos uma variavel para realizar a soma
+    let soma = 0;
+    //resetando a lista de produtos, para ficar vazio de novo
+    listaProdutos.innerHTML ="";
+    // a função map, pode ser considerada um loop para setar os elementos dentro da lista de produtos
+    items.map(function(val){
+        //aqui pegamos os valores de dentros dos inputs e realizamos um parse, para ser considerado um número
+        soma+= parseFloat(val.valor)
+        // setando os elementos dentro da div
+        listaProdutos.innerHTML+= `
+        <div class="lista_de_produtos_single">
+
+        <h3>`+val.nome+`</h3>
+        <h3 class="price_produto"><span>$`+val.valor+`</span></h3>
+        
+        </div>
+        `;
+    })
+    //limitando os valores da soma, para apenas 2 números decimais
+    soma = soma.toFixed(2);
+    //resentando os valores dentro dos inputs
+    nomeProduto.value = "";
+    valorProduto.value = "";
+    //setando os valores de soma, e mostrando isso ao usuário
+    let elementoSoma = document.querySelector('.soma_produto h1')
+    elementoSoma.innerHTML = "R$"+soma;   
+})
+//criando função de limpar 
+document.querySelector("button[name=limpar]")
+.addEventListener("click",()=>{
+    //primeiro dizemos que todos os elementos dentro da lista, irão ser vazios 
+    items = [];
+    // e os elementos dentro tanto da lista, como dos valculos de soma, tbm serão vazios.
+    document.querySelector('.lista_de_produtos').innerHTML = "";
+    document.querySelector('.soma_produto h1').innerHTML = "";
+
+
 })
